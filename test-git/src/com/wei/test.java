6510c710ab56dcbc313f6fb1e7bdb05e8cd01a6d
wei <1069610309@qq.com>
2019-06-15 16:03:49 +0800

    这是我的第一个测试Java

---
+++ test-git/src/com/wei/test.java
@@ -0,0 +1,15 @@
+package com.wei;
+
+/**
+ * @Auther: wei
+ * @Date:2019-06-15
+ * @Description:com.wei
+ * @Version:1.0
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("this id myobject!");
+        System.out.println("this id myobject!");
+        System.out.println("this id myobject!");
+    }
+}
