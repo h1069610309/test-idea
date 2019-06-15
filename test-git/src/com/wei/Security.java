adf3bec752cf97ef7c0a9b6a503be2ceb0543ed8
wei <1069610309@qq.com>
2019-06-15 17:21:10 +0800

    创建新的类

---
+++ test-git/src/com/wei/Security.java
@@ -0,0 +1,13 @@
+package com.wei;
+
+/**
+ * @Auther: wei
+ * @Date:2019-06-15
+ * @Description:com.wei
+ * @Version:1.0
+ */
+public class Security {
+    public static void main(String[] args) {
+        System.out.println("ssss");
+    }
+}
